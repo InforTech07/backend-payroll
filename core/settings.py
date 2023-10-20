a18cf7e029725bd667ef6f1e3ee50803b3058ecc
Infortech07 <Techpilot07@outlook.com>
2023-10-19 21:49:50 -0600

    deploy-test django

--- core/settings.py
+++ core/settings.py
@@ -54,7 +54,7 @@ THIRD_PARTY_APPS=[
     'rest_framework',
     'rest_framework.authtoken',
     'drf_yasg',
-    #'whitenoise',
+    'whitenoise',
 ]
 
 INSTALLED_APPS= DJANGO_APPS + PROJECT_APPS  + THIRD_PARTY_APPS
@@ -63,7 +63,7 @@ INSTALLED_APPS= DJANGO_APPS + PROJECT_APPS  + THIRD_PARTY_APPS
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
